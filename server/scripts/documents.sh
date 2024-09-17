d93e75f7945c2d04942c6827e234c61c977b1886
Richard Bond <richardbond@Richards-MBP.gt.rr.com>
2024-09-17 00:43:49 -0500

    created get create buckets endpoints

--- server/scripts/documents.sh
+++ server/scripts/documents.sh
@@ -1,1 +1,3 @@
-curl -X GET http://localhost:4000/api/document
+# curl -X GET http://localhost:4000/api/document/buckets
+# curl -X POST http://localhost:4000/api/document/bucket/bryce
+curl -X GET http://localhost:4000/api/document/buckets
